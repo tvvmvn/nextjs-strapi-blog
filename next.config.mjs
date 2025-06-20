a898c67695482e5a041d4fb905fe1c3f63403e00
tvvmvn <tvvmvn@gmail.com>
2025-06-20 22:39:59 +0900

    .

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,20 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: [`${process.env.DOMAIN}`]
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '1337',
+        pathname: '/uploads/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lovable-bear-5da4cbf3d6.strapiapp.com', // Replace with your Strapi domain
+        port: '',
+        pathname: '/uploads/**',
+      },
+    ],
   },
 };
 
