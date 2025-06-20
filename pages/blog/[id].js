2f370a953d7cf832f15dd678dad3a57b814deda3
tvvmvn <tvvmvn@gmail.com>
2025-06-20 22:12:22 +0900

    6.20

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,5 +1,7 @@
 import React from "react";
+import { BlocksRenderer } from "@strapi/blocks-react-renderer";
 import Head from "next/head";
+import Image from "next/image";
 import styles from "../../styles/Home.module.css";
 
 export const getStaticPaths = async () => {
@@ -17,28 +19,40 @@ export const getStaticPaths = async () => {
 
 export const getStaticProps = async ({ params }) => {
   
-  const res = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_URL}/blogs/${params.id}`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_URL}/blogs/${params.id}?populate=*`);
   const postData = await res.json();
+
+  console.log(postData.data)
 
   return {
     props: {
-      postData: postData.data
+      postData: postData.data,
+      fileUrl: process.env.FILE_URL
     },
   };
 };
 
-export default function Post({ postData }) {
-  console.log(postData)
-  console.log(postData.draft)
+export default function Post({ postData, fileUrl }) {
+  
   return (
     <>
       <Head>
         <title>{postData.title}</title>
         <meta name="description" content={postData.description} />
       </Head>
-      <h1>{postData.title}</h1>
-      <p>{postData.description}</p>
       <div className={styles.post}>
+        <h1>{postData.title}</h1>
+        <p>{postData.description}</p>
+        
+        <Image
+          src={fileUrl + postData.cover.url}
+          alt="blog-post"
+          priority={true}
+          className="rounded-full"
+          width={600}
+          height={400}
+        />
+        <BlocksRenderer content={postData.draft} />
       </div>
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,23 @@
 import Head from "next/head";
+import Image from "next/image";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 export const getStaticProps = async () => {
-  const resulting = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_URL}/blogs`);
+  const resulting = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_URL}/blogs?populate=*`);
   const result = await resulting.json();
+
+  console.log(result)
+
   return {
     props: {
       result: result.data,
+      fileUrl: process.env.FILE_URL
     },
   };
 };
 
-export default function Home({ result }) {
+export default function Home({ result, fileUrl }) {
   return (
     <>
       <Head>
@@ -20,15 +25,24 @@ export default function Home({ result }) {
         <meta title="description" content="This is an example of our blog" />
       </Head>
       <div className={styles.container}>
-        <h1>Blog Post Links:</h1>
+        <h1>Blog Posts</h1>
+
         <div className={styles.card}>
-          {result.map(result => {
+          {result.map(post => {
             return (
-              <div className={styles.flexing} key={result.id}>
-                <Link href={`/blog/${result.documentId}`}>
-                  <h2>{result.title}</h2>
+              <div className={styles.flexing} key={post.id}>
+                <Link href={`/blog/${post.documentId}`}>
+                  <Image
+                    src={fileUrl + post.cover.url}
+                    alt="blog-post"
+                    priority={true}
+                    className="rounded-full"
+                    width={300}
+                    height={300}
+                  />
+                  <h2>{post.title}</h2>
                   <div>
-                    <p>{result.description}</p>
+                    <p>{post.description}</p>
                   </div>
                 </Link>
               </div>
